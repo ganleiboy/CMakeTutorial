f4e6c4f70bfbb6ab611a5835ecede4a077bc95c5
Lei Gan <35066897+ganleiboy@users.noreply.github.com>
2019-10-17 17:36:47 +0800

    Add files via upload

---
+++ CMakeLists-1测试/test01.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include "test01.h"
+
+using namespace std;
+
+
+void print_hello(){
+	cout << "hello, world." << endl;
+}
+
+
+int main(){
+	print_hello();
+	cout << "===== end =====" << endl;
+	return 0;
+}
---
+++ CMakeLists-1测试/test01.h
@@ -0,0 +1,6 @@
+#ifndef TEST01_H
+#define TEST01_H
+
+void print_hello();
+
+#endif // TEST01_H
